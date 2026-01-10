f3702d17b1189df7ed5f777df68b8b457fb288e9
FernandoPassaia VivobookFedora <fernandopassaia@futuradata.com.br>
2026-01-09 22:25:33 -0300

    Starting Galego's LeetCode Course

---
+++ 01_Workshop10LeetCode/C#/Algorithms/TwpSum.cs
@@ -0,0 +1,16 @@
+// @ts-nocheck
+// vscode-disable-inline-completions
+public class TwoSum()
+{
+
+    public int[] TwoSumUsingON2AndBrutalForce(int[] inputs, int target)
+    {
+
+    }
+
+    public int[] TwoSumUsingONAndHashmap(int[] inputs, int target)
+    {
+
+    }
+
+}
